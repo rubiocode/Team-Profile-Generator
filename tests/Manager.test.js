e8b936697447308619bbdc76e659ee8245a3e784
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-07-08 19:19:40 -0700

    created Manager tests, based on tests developed Manager code class and methods

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,3 +1,20 @@
+const { expect } = require("@jest/globals");
 const Manager = require("../lib/Manager");
-const Employee = require("../lib/Employee");
 
+test ("Creates office number using constructorfunction", () => {
+    const officeNumber = 720;
+    const employee = new Manager("Lidia", 100, "test@example.com", officeNumber);
+    expect(employee.officeNumber).toBe(officeNumber);
+});
+
+test ("getOfficeNumber() returns office number", () =>{
+    const officeNumber = 720;
+    const employee = new Manager("Lidia", 100, "test@example.com", officeNumber)
+    expect(employee.getOfficeNumber()).toBe(officeNumber);
+});
+
+test ("getRole() returns \'Manager\'", () =>{
+    const role= "Manager";
+    const employee = new Manager("Lidia", 100, "test@example.com", 720)
+    expect (employee.getRole()).toBe(role);
+});
